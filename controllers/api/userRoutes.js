1844f4ccc1d2241323a63482390b39f361849934
Katlyn Boches <86095070+Katlyn627@users.noreply.github.com>
2022-01-06 18:55:14 -0700

    updated user routes and login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,25 +1,32 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+
+// CREATE new user
 router.post('/', async (req, res) => {
-  try {
-    const userData = await User.create(req.body);
+    try {
+      const userData = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+  
+      req.session.save(() => {
+        req.session.loggedIn = true;
+  
+        res.status(200).json(userData);
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
 
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
 
-      res.status(200).json(userData);
-    });
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-router.post('/login', async (req, res) => {
+router.post('/:id', async (req, res) => {
   try {
     const userData = await User.findOne(
-        { where: { email: req.body.email } });
+        { where: { id: req.body.id } });
 
     if (!userData) {
       res
