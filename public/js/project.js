1b478c994cae51bc4c2e2da618486e3e7c3a1394
Katlyn Boches <86095070+Katlyn627@users.noreply.github.com>
2022-01-13 23:52:23 -0700

    updated login and project

--- public/js/project.js
+++ public/js/project.js
@@ -1,13 +1,47 @@
-
-function deleteProjectFormHandler(){
-// Grab user_id from data attribute in profile views
-// Make a fetch call to delete post using that id
-
-
-}
-
-
-
-document
-  .querySelector('.delete-btn')
-  .addEventListener('click', deleteProjectFormHandler);
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const name = document.querySelector('#project-name').value.trim();
+    // const needed_funding = document.querySelector('#project-funding').value.trim();
+    const description = document.querySelector('#project-desc').value.trim();
+  
+    if (name && description) {
+      const response = await fetch(`/api/projects`, {
+        method: 'POST',
+        body: JSON.stringify({ name, description }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to create project');
+      }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/projects/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to delete project');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new-project-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.project-list')
+    .addEventListener('click', delButtonHandler);
